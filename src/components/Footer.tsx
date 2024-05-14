098a645f0b4320d00fac4c57fc55bb34852e0c6f
matt whalley <mttwhlly@icloud.com>
2024-05-14 16:38:50 -0400

    Add some basic components, pages, queries and mutations

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+/**
+ * Footer component.
+ */
+export default function Footer() {
+  return (
+    <footer className="text-sm text-center border-t-2 p-8">
+      &copy; 2018-{new Date().getFullYear()} Hatch Creatives
+    </footer>
+  );
+}
---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+const config = {
+    siteName: 'Hatch Creatives',
+    siteDescription: "",
+    siteUrl: 'https://hatchcreatives.com'
+  }
+  
+  export default config
