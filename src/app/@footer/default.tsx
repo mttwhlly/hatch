1cfd0a7549946e30e65e095114fa1fb74aeab1fe
matt whalley <mttwhlly@icloud.com>
2024-09-04 12:14:46 -0400

    feat: Add new UI component for aspect ratio and date formatting
    This commit adds two new files: `aspect-ratio.tsx` and `date.tsx`. The `aspect-ratio.tsx` file exports a new UI component called `AspectRatio` from the `@radix-ui/react-aspect-ratio` package. The `date.tsx` file exports a new function component called `Date` which takes a `dateString` prop and formats it using the `date-fns` library.
    
    These changes introduce new functionality to the project and improve the user experience by providing a reusable aspect ratio component and a date formatting utility.

--- src/app/@footer/default.tsx
+++ src/app/@footer/default.tsx
@@ -3,20 +3,80 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t py-6 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Image src="/logo.svg" alt="Logo" width={80} height={26} />
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            &copy; 2018-{new Date().getFullYear()} Hatch Creatives. All rights
-            reserved.
-          </p>
+    <footer className="w-full pt-8 bg-brown-700 text-white flex flex-col">
+      <div className="max-w-4xl container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row px-8 lg:px-0">
+        <div className="flex flex-col items-center md:px-0">
+          <div className="flex justify-center mb-4">
+            <Link href="/">
+              <svg
+                width="46"
+                height="48"
+                viewBox="0 0 46 48"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M31.944 28.912C30.504 32.816 29.192 34.512 26.568 34.512C24.136 34.512 22.824 33.232 22.824 31.28C22.824 30.512 23.048 29.616 23.4 28.656L25.64 22.672C25.992 21.712 26.216 20.976 26.216 20.368C26.216 19.696 25.832 19.28 25.064 19.28C23.336 19.28 21.224 22.256 19.528 28.048L17.768 34C17.512 34 16.712 33.968 15.432 33.968C14.152 33.968 13.352 34 13.064 34L18.952 13.776C19.624 11.536 19.432 11.184 17.992 11.024L16.616 10.864V10.48L24.872 9.136L25.224 9.36C24.712 10.544 24.232 11.888 23.752 13.424L22.792 16.752C21.64 20.656 20.712 22.544 19.08 27.952C20.904 22 22.888 17.776 26.984 17.776C29.256 17.776 30.504 19.152 30.504 21.104C30.504 21.968 30.312 22.832 29.96 23.76L27.592 30.16C27.336 30.864 27.08 31.568 27.08 32.208C27.08 32.88 27.4 33.296 28.072 33.296C29.032 33.296 30.12 32.24 31.4 28.752L31.944 28.912Z"
+                  fill="white"
+                />
+                <circle cx="23" cy="23" r="22.5" stroke="#E8C6BA" />
+              </svg>
+            </Link>
+          </div>
         </div>
-        <nav className="flex items-center space-x-4 text-sm">
-          <Link href="/terms">Terms</Link>
-          <Link href="/privacy">Privacy</Link>
-          <Link href="/contact">Contact</Link>
-        </nav>
+      </div>
+      <nav className="max-w-4xl container flex text-xs flex-row px-8 lg:px-0 pb-8 font-sans uppercase tracking-widest text-brown-200">
+        <div className="flex flex-col md:flex-row w-full md:w-1/2">
+          <div className="flex flex-col mx-0 my-4 gap-2 w-full md:w-1/2">
+            <Link className="h-6 hover:underline" href="/about">
+              About
+            </Link>
+            <Link className="h-6 hover:underline" href="/community">
+              Community
+            </Link>
+            <Link className="h-6 hover:underline" href="/job-board">
+              Job Board
+            </Link>
+            <Link className="h-6 hover:underline" href="/blog">
+              Blog
+            </Link>
+          </div>
+          <div className="flex flex-col mx-0 my-4 gap-2 w-full md:w-1/2">
+            <Link className="h-6 hover:underline" href="/contact">
+              Contact
+            </Link>
+            <Link className="h-6 hover:underline" href="/privacy">
+              Privacy & Policy
+            </Link>
+            <Link className="h-6 hover:underline" href="/terms">
+              Terms & Conditions
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row w-full md:w-1/2">
+          <div className="flex flex-col mx-0 my-4 gap-2 w-full md:w-1/2">
+            <Link className="h-6 hover:underline" href="/host">
+              Host a Meetup
+            </Link>
+          </div>
+          <div className="flex flex-col mx-0 my-4 gap-2 w-full md:w-1/2">
+            <Link className="h-6 hover:underline" href="/else">
+              Something Else
+            </Link>
+            <Link className="h-6 hover:underline" href="/else">
+              Something Else
+            </Link>
+            <Link className="h-6 hover:underline" href="/else">
+              Something Else
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <div className="w-full container p-0">
+        <p className="text-center text-[10px] uppercase font-sans leading-loose tracking-wider bg-brown-900 text-brown-400">
+          Copyright &copy; 2018-{new Date().getFullYear()} Hatch Creatives. All
+          rights reserved.
+        </p>
       </div>
     </footer>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,21 @@ import { Post } from "@/lib/types";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import Date from "@/components/date";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Input } from "@/components/ui/input";
+import InstaFeed from "@/components/instafeed";
+import { Heart, HandHeart, HeartHandshake, MailOpen } from "lucide-react";
 
 /**
  * The homepage route.
@@ -35,18 +40,19 @@ export default async function Home() {
   }
 
   return (
-    <main className="max-w-3xl flex-1">
+    <main className="flex-1">
       {/* Hero section */}
-      <section className="w-full h-[600px] relative flex items-center justify-center">
+      <section className="h-[200px] md:h-[300px] lg:h-[540px] relative flex items-center justify-center">
         <Image
-          src="/hero-image.jpg"
+          src="/hero.jpg"
           alt="Hero background"
           layout="fill"
           objectFit="cover"
           quality={100}
+          sizes="100vw"
           priority
         />
-        <div className="relative z-10 text-center text-white">
+        {/* <div className="relative z-10 text-center text-white">
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl mb-4">
             Welcome to Our Website
           </h1>
@@ -68,105 +74,203 @@ export default async function Home() {
               Learn More
             </Button>
           </div>
-        </div>
+        </div> */}
       </section>
 
-      {/* Horizontally scrollable cards */}
-      <section className="w-full py-12 bg-muted">
-        <div className="container px-4 md:px-6">
-          <h2 className="text-2xl font-bold tracking-tighter mb-4">
-            Featured Content
-          </h2>
-          <ScrollArea className="w-full whitespace-nowrap rounded-md border">
-            <div className="flex w-max space-x-4 p-4">
-              {[1, 2, 3].map((i) => (
-                <Card key={i} className="w-[250px] h-[250px]">
-                  <CardHeader>
-                    <CardTitle>Card {i}</CardTitle>
-                    <CardDescription>This is a scrollable card</CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <p>Content for Card {i}</p>
-                  </CardContent>
-                </Card>
-              ))}
-            </div>
-            <ScrollBar orientation="horizontal" />
-          </ScrollArea>
-        </div>
-      </section>
       <article>
-        <h1 dangerouslySetInnerHTML={{ __html: homepage.title }} />
-        <div dangerouslySetInnerHTML={{ __html: homepage.content }} />
+        <div className="max-w-2xl mx-auto my-3 container px-4 md:px-6 py-12">
+          <h1 className="font-serif text-4xl md:text-5xl lg:text-6xl text-center tracking-tight leading-normal font-normal p-2 text-neutral-700">
+            we’re <span className="italic">hatch gals</span>
+          </h1>
+          <p className="text-center text-lg py-3 text-pretty text-neutral-700">
+            Hatch Creatives provides space and opportunities for gals to come
+            together as friends, coworkers and community members.
+          </p>
+          {/* <h1 dangerouslySetInnerHTML={{ __html: homepage.title }} />
+          <div dangerouslySetInnerHTML={{ __html: homepage.content }} /> */}
+        </div>
       </article>
 
-      <aside>
-        <h2>Latest Posts</h2>
-        <div className="flex flex-wrap gap-8">
-          {posts.map((post: Post) => (
-            <article className="w-72" key={post.databaseId}>
-              <Image
-                alt={post.featuredImage.node.altText}
-                height={post.featuredImage.node.mediaDetails.height}
-                src={post.featuredImage.node.sourceUrl}
-                width={post.featuredImage.node.mediaDetails.width}
-                priority={true}
-              />
-              <Link href={`/blog/${post.slug}`}>
-                <h2 dangerouslySetInnerHTML={{ __html: post.title }} />
-              </Link>
-              <p className="text-sm text-gray-500">
-                {post.commentCount} Comments
-              </p>
-              <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-              <Link className="button" href={`/blog/${post.slug}`}>
-                View Post
-              </Link>
-            </article>
-          ))}
-        </div>
-      </aside>
-      {/* Instagram feed */}
-      <section className="w-full py-12">
-        <div className="container px-4 md:px-6">
-          <h2 className="text-2xl font-bold tracking-tighter mb-4">
-            Instagram Feed
-          </h2>
-          <ScrollArea className="w-full whitespace-nowrap rounded-md border">
-            <div className="flex w-max space-x-4 p-4">
-              {[1, 2, 3, 4, 5, 6].map((i) => (
-                <div key={i} className="w-[300px] h-[300px] relative">
-                  <Image
-                    src={`/instagram-${i}.jpg`}
-                    alt={`Instagram post ${i}`}
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-md"
-                  />
+      {/* Horizontally scrollable cards */}
+      <section className="py-12 bg-muted">
+        <div className="max-w-4xl mx-auto container px-4 md:px-6">
+          <div className="flex space-x-4">
+            <Card className="w-[300px] h-[300px] text-wrap text-sm bg-brown-200">
+              <CardHeader>
+                <div className="flex flex-col items-center space-y-4 pb-4">
+                  <Heart size={64} strokeWidth={1} color="white" />
                 </div>
-              ))}
-            </div>
-            <ScrollBar orientation="horizontal" />
-          </ScrollArea>
+                <CardTitle className="text-lg tracking-normal text-center text-brown-900">
+                  encouragement
+                </CardTitle>
+                <CardDescription className="text-wrap text-center text-brown-800"></CardDescription>
+              </CardHeader>
+              <CardContent className="text-sm text-center text-brown-800">
+                <p>
+                  We learn new skills, ask for and offer support, and stoke the
+                  passions inside us. We celebrate and cheer on the steps and
+                  successes of others.
+                </p>
+              </CardContent>
+            </Card>
+            <Card className="w-[300px] h-[300px] text-wrap text-sm bg-blue-200">
+              <CardHeader>
+                <div className="flex flex-col items-center space-y-4 pb-4">
+                  <HandHeart size={64} strokeWidth={1} color="white" />
+                </div>
+                <CardTitle className="text-lg tracking-normal text-center text-blue-900">
+                  collaboration
+                </CardTitle>
+                <CardDescription className="text-wrap text-center text-blue-800"></CardDescription>
+              </CardHeader>
+              <CardContent className="text-sm text-center text-blue-800">
+                <p>
+                  We engage with other women and discover opportunities to
+                  collaborate in new and innovative ventures.
+                </p>
+              </CardContent>
+            </Card>
+
+            <Card className="w-[300px] h-[300px] text-wrap text-sm bg-yellow-200">
+              <CardHeader>
+                <div className="flex flex-col items-center space-y-4 pb-4">
+                  <HeartHandshake size={64} strokeWidth={1} color="white" />
+                </div>
+                <CardTitle className="text-lg tracking-normal text-center text-yellow-900">
+                  accountability
+                </CardTitle>
+                <CardDescription className="text-wrap text-center text-yellow-800"></CardDescription>
+              </CardHeader>
+              <CardContent className="text-sm text-center text-yellow-800">
+                <p>
+                  We seek accountability in order to bring our dreams and
+                  ambitions to their full potential. We come alongside each
+                  other as friends, mentors, and project partners.
+                </p>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </section>
+
+      <section className="max-w-4xl mx-auto my-14 container px-4 md:px-6">
+        <Card className="w-full text-wrap text-sm bg-brown-300 flex flex-col md:flex-row">
+          <AspectRatio ratio={16 / 9}>
+            <Image
+              src="/hero.jpg"
+              alt="Hero background"
+              layout="fill"
+              sizes="100vw"
+              objectFit="cover"
+            />
+          </AspectRatio>
+          <div className="flex flex-col justify-center">
+            <CardHeader>
+              <CardTitle className="tracking-tight text-3xl font-light text-center text-brown-900">
+                let’s get together <br />{" "}
+                <span className="italic">yeah, yeah, yeah</span>
+              </CardTitle>
+              <CardDescription className="text-wrap text-center text-brown-800">
+                We learn new skills, ask for and offer support, and stoke the
+                passions inside us.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="text-sm text-center">
+              <Button className="font-normal tracking-widest bg-brown-700">
+                Upcoming Meetups
+              </Button>
+            </CardContent>
+          </div>
+        </Card>
+      </section>
+
+      <aside className="bg-yellow-200 mt-8 py-8">
+        <div className="max-w-4xl mx-auto container px-4">
+          <div className="max-w-4xl mx-auto">
+            <ScrollArea className="w-full whitespace-nowrap">
+              <div className="flex w-max space-x-4 px-2 py-8">
+                {posts.map((post: Post, i) => (
+                  <Card key={i} className="w-full text-wrap text-sm">
+                    <AspectRatio ratio={16 / 9}>
+                      <Image
+                        alt={post.featuredImage.node.altText}
+                        layout="fill"
+                        sizes="400px"
+                        objectFit="cover"
+                        src={post.featuredImage.node.sourceUrl}
+                        priority={true}
+                      />
+                    </AspectRatio>
+                    <CardHeader className="text-base hover:underline p-3">
+                      <Link href={`/blog/${post.slug}`}>
+                        <h2
+                          className="text-lg font-semibold tracking-tight mb-4 text-pretty"
+                          dangerouslySetInnerHTML={{ __html: post.title }}
+                        />
+                      </Link>
+                    </CardHeader>
+                    <CardContent className="text-sm p-3 text-gray-500">
+                      <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+          </div>
+        </div>
+      </aside>
 
       {/* Email signup form */}
       <section className="w-full py-12">
-        <div className="container px-4 md:px-6">
+        <div className="max-w-4xl mx-auto container py-8 px-4 md:px-6 bg-brown-300">
+          <div className="flex flex-col items-center space-y-4 pb-4">
+            <MailOpen size={64} strokeWidth={1} color="white" />
+          </div>
           <div className="flex flex-col items-center space-y-4 text-center">
-            <h2 className="text-2xl font-bold tracking-tighter">
+            <h2 className="text-3xl font-light tracking-tight text-brown-900">
               Subscribe to Our Newsletter
             </h2>
-            <p className="text-muted-foreground">
-              Stay updated with our latest news and offers
-            </p>
-            <div className="flex w-full max-w-sm items-center space-x-2">
-              <Input type="email" placeholder="Enter your email" />
-              <Button type="submit">Subscribe</Button>
+            <p className="text-brown-800">no spam ever. we promise.</p>
+            <div className="flex w-full max-w-sm items-center">
+              <Input
+                className="border-none"
+                type="email"
+                placeholder="Enter your email"
+              />
+              <Button
+                className="font-normal tracking-widest bg-brown-900"
+                type="submit"
+              >
+                Subscribe
+              </Button>
             </div>
           </div>
         </div>
+      </section>
+
+      {/* Instagram feed */}
+      <InstaFeed />
+      <section className="">
+        {/* <h2 className="text-2xl font-bold tracking-tighter mb-4">
+            Follow us on Instagram
+          </h2> */}
+        <ScrollArea className="w-full whitespace-nowrap">
+          <div className="flex w-max">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => (
+              <div key={i} className="w-[180px] h-[180px] relative">
+                <Image
+                  src={`/hero.jpg`}
+                  alt={`Instagram post ${i}`}
+                  layout="fill"
+                  objectFit="cover"
+                  className=""
+                />
+              </div>
+            ))}
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </section>
     </main>
   );
